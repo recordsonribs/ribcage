d3145b9e915ab358a3798de125a0b84deb6f7c78
ribcage <ribcage@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2007-11-26 14:27:49 +0000

    Added a few templates that should exist as they are directed to by ribcage.php.

---
+++ templates/bio.php
@@ -0,0 +1,28 @@
+<?php get_header(); ?>
+<div id="container">
+		<div id="content">
+	 
+	<h2><a href="<?php echo ARTISTS_PLUGIN; ?>">Artists</a></h2>
+	<a href="<?php echo ARTISTS_PLUGIN; ?><?php artist_slug(); ?>"><h1><?php artist_name(); ?></h1></a>
+	<?php if (is_artist_page()) : ?>
+	<?php else : ?>
+		<p><?php artist_bio(); ?></p>
+		<p>
+		<?php if (artist_website_link(0)) : ?><a href="<?php artist_website_link(); ?>">Offical Webpage</a><br /><?php endif ?>
+		<?php if (artist_myspace_link(0)) : ?><a href="<?php artist_myspace_link(); ?>">My Space</a><br /><?php endif ?>
+		<?php if (artist_facebook_link(0)) : ?><a href="<?php artist_facebook_link(); ?>">Facebook</a><br /><?php endif ?>
+		<a href="<?php artist_lastfm_link(); ?>">Last.fm</a><br />
+		<a href="<?php artist_press_link(); ?>">Press Photos and Information</a></p>
+	<?php endif; ?>
+	<?php if (have_releases() ) : ?>
+	<?php if (!is_artist_page() ) : ?><h2>Releases</h2><?php endif; ?>
+	<?php while ( have_releases  () ) : the_release (); ?>
+	<div>
+	<h3><a href="<?php get_option('siteurl').'/artists/'.$artist['artist_slug'].release_slug(); ?>"><?php release_title(); ?></a></h3>
+	</div>
+	<?php endwhile; ?>
+	<?php endif; ?>
+</div>
+</div>
+<?php get_sidebar(); ?>
+<?php get_footer();?>
