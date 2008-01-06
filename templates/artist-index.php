6db4fb93658287892b315ff9761a6e40fe4805f2
ribcage <ribcage@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2008-01-06 22:01:42 +0000

    Why was I using a constant like a silly billy?

--- templates/artist-index.php
+++ templates/artist-index.php
@@ -2,9 +2,9 @@
 <!-- Add a microformats dumb ass!!!-->
 <div id="container">
 	<div id="content">
-		<h2><a href="<?php echo ARTISTS_PLUGIN; ?>" title="Return to all our artists.">Artists</a></h2>
+		<h2><a href="<?php echo get_option('siteurl'); ?>/artists/" title="Return to all our artists.">Artists</a></h2>
 <?php while ( have_artists () ) : the_artist(); ?>	
-		<a href="<?php echo ARTISTS_PLUGIN; ?><?php artist_slug(); ?>" title="<?php artist_name(); ?>"><h1><?php artist_name(); ?></h1></a>
+		<a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>" title="<?php artist_name(); ?>"><h1><?php artist_name(); ?></h1></a>
 		<p><?php artist_blurb_tiny(); ?></p>
 <?php endwhile; ?>
 	</div>
