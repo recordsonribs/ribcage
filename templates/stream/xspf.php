654c0def68b12c2c504c8944f22eb7bce8f5d165
ribcage <ribcage@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2008-01-08 14:37:04 +0000

    Messed up...

--- templates/stream/xspf.php
+++ templates/stream/xspf.php
@@ -9,7 +9,6 @@
 			<location><?php track_stream(); ?></location>
 			<title><?php track_title(); ?></title>
 			<image><?php release_cover_tiny(); ?></image>
-			<id><?php track_id(); ?></id>
 		</track>
 <?php endwhile; ?>
 	</trackList>
