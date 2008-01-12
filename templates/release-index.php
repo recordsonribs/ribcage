32086b98e9cacec6c31656719e4ca52a5dd97f12
ribcage <ribcage@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2008-01-12 19:37:10 +0000

    ...

--- templates/release-index.php
+++ templates/release-index.php
@@ -17,7 +17,7 @@ eval("page" + id + " = window.open(URL, '" + id + "', 'toolbar=0,scrollbars=0,lo
 				<h2 class="entry-title">Releases</h2>
 				<div class="entry-content">
 					<?php while ( have_releases () ) : the_release() ; ?>
-					<img src="<?php release_cover_tiny ();?>" align="right" style="border-left: 20px;" />
+					<img src="<?php release_cover_tiny ();?>" align="right" style="margin-left: 20px; border" />
 					<h3><?php echo get_artistname_by_id($release['release_artist']); ?> - <?php release_title(); ?></h3>
 					<p><?php release_blurb_short(); ?></p>
 					<p><a>More Information</a> - <a href="javascript:popUp('<?php release_player_link (); ?>')">Listen Now</a> - Free Download</p>
