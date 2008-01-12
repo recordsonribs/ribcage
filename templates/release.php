d8efda038ae88feb143f03d30d24619982ab6676
ribcage <ribcage@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2008-01-12 23:27:57 +0000

    Image support added to release template

--- templates/release.php
+++ templates/release.php
@@ -13,7 +13,7 @@ eval("page" + id + " = window.open(URL, '" + id + "', 'toolbar=0,scrollbars=0,lo
 		<div id="post-17" class="hentry p1 page publish author-alex category-uncategorized tag- y2007 m07 d19 h23">
 			<h2 class="entry-title"><a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>"><?php artist_name(); ?></a> - <a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>/<?php release_slug(); ?>"><?php release_title(); ?></a></h2>
 			<div class="entry-content">
-				<img src="<?php release_cover_large (); ?>" />
+				<img src="<?php release_cover_large (); ?>" align="right" style="margin-left: 20px; border: 1px solid #000;" />
 				<p>
 				<?php while ( have_tracks () ) : the_track() ; ?>
 				<?php track_no(); ?>. <?php track_title(); ?> (<?php track_time(); ?>)<br />
