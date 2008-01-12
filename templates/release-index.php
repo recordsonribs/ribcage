f63c08ddfc67fb944880a75ac76a53869a376f01
ribcage <ribcage@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2008-01-12 18:28:27 +0000

    Further theme related fun.

--- templates/release-index.php
+++ templates/release-index.php
@@ -11,16 +11,23 @@ eval("page" + id + " = window.open(URL, '" + id + "', 'toolbar=0,scrollbars=0,lo
 }
 // End -->
 </script>
-<div id="container">
-	<h2><a href="releases/">Releases</a></h2>
+<?php get_header() ?>
+
+	<div id="container">
+		<div id="content">
+			<div id="post-<?php the_ID(); ?>" class="<?php sandbox_post_class() ?>">
+				<h2 class="entry-title"><a>Releases</a></h2>
+				
 	<?php while ( have_releases () ) : the_release() ; ?>
-	<div>
 	<img src="<?php release_cover_tiny ();?>" align="right" />
 	<h3><?php echo get_artistname_by_id($release['release_artist']); ?> - <?php release_title(); ?></h3>
 	<p><?php release_blurb_short(); ?></p>
 	<p><a href="javascript:popUp('<?php release_player_link (); ?>')">Listen Now</a></p>
-	</div>
 	<?php endwhile; ?>
-</div>
+	</div>
+</div><!-- .post -->
+
+</div><!-- #content -->
+</div><!-- #container -->
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
