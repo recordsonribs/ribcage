421dcb9e4819770244ba98ebb203a866aae4157e
ribcage <ribcage@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2008-01-12 18:35:01 +0000

    ,,,

--- templates/release-index.php
+++ templates/release-index.php
@@ -1,15 +1,21 @@
+<?php
+global $releases, $release;
+?>
 <?php get_header() ?>
-
 	<div id="container">
 		<div id="content">
 			<div id="post-" class="<?php sandbox_post_class() ?>">
 				<h2 class="entry-title">Releases</h2>
 				<div class="entry-content">
-
+					<?php while ( have_releases () ) : the_release() ; ?>
+					<img src="<?php release_cover_tiny ();?>" align="right" />
+					<h3><?php echo get_artistname_by_id($release['release_artist']); ?> - <?php release_title(); ?></h3>
+					<p><?php release_blurb_short(); ?></p>
+					<p><a href="javascript:popUp('<?php release_player_link (); ?>')">Listen Now</a></p>
+					<?php endwhile; ?>
 				</div>
 			</div><!-- .post -->
 		</div><!-- #content -->
 	</div><!-- #container -->
-
 <?php get_sidebar() ?>
 <?php get_footer() ?>
