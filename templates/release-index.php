9f3efd1b05e5f97eae4d451a81e46cc8d71b3000
ribcage <ribcage@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2008-01-12 18:33:32 +0000

    ...

--- templates/release-index.php
+++ templates/release-index.php
@@ -1,32 +1,15 @@
-<?php get_header(); ?>
-<?php
-global $releases, $release;
-?>
-<SCRIPT LANGUAGE="JavaScript">
-<!-- Begin
-function popUp(URL) {
-day = new Date();
-id = day.getTime();
-eval("page" + id + " = window.open(URL, '" + id + "', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=320,height=140,left = 560,top = 410');");
-}
-// End -->
-</script>
+<?php get_header() ?>
+
 	<div id="container">
 		<div id="content">
-			<div id="post-12" class="<?php sandbox_post_class() ?>">
-				<h2 class="entry-title"><a>Releases</a></h2>
-				
-	<?php while ( have_releases () ) : the_release() ; ?>
-	<img src="<?php release_cover_tiny ();?>" align="right" />
-	<h3><?php echo get_artistname_by_id($release['release_artist']); ?> - <?php release_title(); ?></h3>
-	<p><?php release_blurb_short(); ?></p>
-	<p><a href="javascript:popUp('<?php release_player_link (); ?>')">Listen Now</a></p>
-	<?php endwhile; ?>
-	</div>
-</div><!-- .post -->
+			<div id="post-" class="<?php sandbox_post_class() ?>">
+				<h2 class="entry-title">Releases</h2>
+				<div class="entry-content">
 
-</div><!-- #content -->
-</div><!-- #container -->
+				</div>
+			</div><!-- .post -->
+		</div><!-- #content -->
+	</div><!-- #container -->
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php get_sidebar() ?>
+<?php get_footer() ?>
