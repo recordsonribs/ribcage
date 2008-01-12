f721e589a0edc92ec4d60e59ce55d6c4c7320e6a
ribcage <ribcage@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2008-01-12 18:44:05 +0000

    ...

--- templates/artist-index.php
+++ templates/artist-index.php
@@ -3,15 +3,15 @@
 <div id="container">
 	<div id="content">
 		<div id="post-17" class="hentry p1 page publish author-alex category-uncategorized tag- y2007 m07 d19 h23">
-		<h2><a href="<?php echo get_option('siteurl'); ?>/artists/" title="Return to all our artists.">Artists</a></h2>
+			<h2 class="entry-title"><a href="<?php echo get_option('siteurl'); ?>/artists/" title="Return to all our artists.">Artists</a></h2>
 		<div class="entry-content">
 <?php while ( have_artists () ) : the_artist(); ?>	
 		<a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>" title="<?php artist_name(); ?>"><h3><?php artist_name(); ?></h3></a>
 		<p><?php artist_blurb_tiny(); ?></p>
 <?php endwhile; ?>
-</div>
-</div>
-	</div>
-</div>
+			</div><!-- .entry-content -->
+		</div><!-- .post -->
+	</div><!-- #content -->
+</div><!-- #container -->
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
