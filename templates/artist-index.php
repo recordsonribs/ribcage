e6888e83220431672ff6232b8c179b4d67294c12
ribcage <ribcage@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2008-01-12 18:42:02 +0000

    More theme updating...

--- templates/artist-index.php
+++ templates/artist-index.php
@@ -2,11 +2,15 @@
 <!-- Add a microformats dumb ass!!!-->
 <div id="container">
 	<div id="content">
+		<div id="post-17" class="hentry p1 page publish author-alex category-uncategorized tag- y2007 m07 d19 h23">
 		<h2><a href="<?php echo get_option('siteurl'); ?>/artists/" title="Return to all our artists.">Artists</a></h2>
+		<div class="entry-content">
 <?php while ( have_artists () ) : the_artist(); ?>	
-		<a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>" title="<?php artist_name(); ?>"><h1><?php artist_name(); ?></h1></a>
+		<a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>" title="<?php artist_name(); ?>"><h3><?php artist_name(); ?></h3></a>
 		<p><?php artist_blurb_tiny(); ?></p>
 <?php endwhile; ?>
+</div>
+</div>
 	</div>
 </div>
 <?php get_sidebar(); ?>
