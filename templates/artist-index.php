f5e27c8abf2052f34cad5cca8f50a925e36f82da
ribcage <ribcage@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2008-01-13 02:15:08 +0000

    sp

--- templates/artist-index.php
+++ templates/artist-index.php
@@ -7,7 +7,7 @@
 		
 <?php while ( have_artists () ) : the_artist(); ?>
 	<div class="entry-content">
-		<img src="<?php artist_thumb ();?>" align="right" style="margin-bottom: 20px; margin-left: 2opx; border: 1px solid #000; clear:both;" />
+		<img src="<?php artist_thumb ();?>" align="right" style="margin-bottom: 20px; margin-left: 20px; border: 1px solid #000; clear:both;" />
 		<a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>" title="<?php artist_name(); ?>"><h3><?php artist_name(); ?></h3></a>
 		<p><?php artist_blurb_tiny(); ?></p>
 		</div>
