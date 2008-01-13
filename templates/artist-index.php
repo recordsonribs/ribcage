42298555f98839434c591754b9278b4a22de9ff8
ribcage <ribcage@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2008-01-13 02:36:15 +0000

    More template fun...

--- templates/artist-index.php
+++ templates/artist-index.php
@@ -6,7 +6,7 @@
 			<h2 class="entry-title"><a href="<?php echo get_option('siteurl'); ?>/artists/" title="Return to all our artists.">Artists</a></h2>
 		
 <?php while ( have_artists () ) : the_artist(); ?>
-	<div class="entry-content" style="height=175px;" >
+	<div class="entry-content" style="height=175px; border-bottom:20px;" >
 		<img src="<?php artist_thumb ();?>" align="right" style="margin-left: 20px; border: 1px solid #000; clear:both;" />
 		<a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>" title="<?php artist_name(); ?>"><h3><?php artist_name(); ?></h3></a>
 		<p><?php artist_blurb_tiny(); ?></p>
