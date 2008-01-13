908f44484c05b7b61f2365692218e8393619dea9
ribcage <ribcage@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2008-01-13 16:20:22 +0000

    Mended my own mod_rewrite stupidity

--- ribcage.php
+++ ribcage.php
@@ -214,8 +214,8 @@ function ribcage_add_rewrite_rules ( $wp_rewrite ) {
 		
 		"(player)/(.*)" => 'index.php?ribcage_player=1&release_slug='.$wp_rewrite->preg_index(2),
 		
-		"(buy)" => 'index.php?ribcage_buy=1',
-		"(buy)/(.*)" => 'index.php?ribcage_buy=1&ribcage_item_id='.$wp_rewrite->preg_index(2)
+		"(buy)/(.*)" => 'index.php?ribcage_buy=1&ribcage_item_id='.$wp_rewrite->preg_index(2),
+		"(buy)" => 'index.php?ribcage_buy=1'
 	);
 
 	$wp_rewrite->rules = $wp_rewrite->rules + $new_rules;
