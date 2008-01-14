6885197a6b50f9f20810874dec9b6b1ad6946218
ribcage <ribcage@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2008-01-14 11:26:09 +0000

    Only an idiot could do a div so wrong...

--- templates/artist-index.php
+++ templates/artist-index.php
@@ -2,11 +2,11 @@
 <div id="container">
 	<div id="content">
 		<div id="post-17" class="hentry p1 page publish author-alex category-uncategorized tag- y2007 m07 d19 h23">
-			<h2 class="entry-title"><a href="<?php echo get_option('siteurl'); ?>/artists/" title="Return to all our artists.">Artists</a></h2>
+			<h2 class="entry-title"><a href="<?php echo get_option('siteurl'); ?>/artists/" title="Artists.">Artists</a></h2>
 			<div class="entry-content">
 <?php while ( have_artists () ) : the_artist(); ?>
-		<div style="ribcage-artist">
-		<a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>" title="<?php artist_name(); ?>"><img src="<?php artist_thumb ();?>" align="right" style="margin-left: 20px; border: 1px solid #000;" /></a>
+		<div class="ribcage-artist">
+		<a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>" title="<?php artist_name(); ?>"><img src="<?php artist_thumb ();?>" style="float: right; margin-left: 20px; border: 1px solid #000;" /></a>
 		<a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>" title="<?php artist_name(); ?>"><h3><?php artist_name(); ?></h3></a>
 		<p><?php artist_blurb_tiny(); ?></p>
 		</div>
