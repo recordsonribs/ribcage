3b52bf95a7797ec321c31bc33fdf50e5de6be7a5
ribcage <ribcage@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2008-01-14 18:03:29 +0000

    dd

--- templates/thanks.php
+++ templates/thanks.php
@@ -2,13 +2,15 @@
 <div id="container">
 	<div id="content">
 		<div id="post-17" class="hentry p1 page publish author-alex category-uncategorized tag- y2007 m07 d19 h23">
-			<h2 class="entry-title"><a href="<?php get_option('siteurl') ?>/buy/<?php product_id(); ?>">Buying <?php product_name(); ?></a><h2>
+			<h2 class="entry-title"><a href="<?php get_option('siteurl') ?>/buy/<?php product_id(); ?>">Thanks <?php echo $_POST['first_name'];?></a><h2>
 				<div class="entry-content">
-					<p><?php product_description(); ?></p>
-					<h3>Pricing</h3>
-					<p>CD Album & Postage (UK) - £<?php product_cost_uk(); ?><br />
-						CD Album & Postage (Worldwide)- £<?php product_cost_ww(); ?></p>
-					<p><a href="<?php get_option('siteurl') ?>/buy/<?php product_id(); ?>/go">Buy Now</a></p>
+					<p>Thanks very much for your order. We'll get back to you as soon as possible.</p>
+					<h3>Details</h3>
+					<p>
+					<?php echo $_POST['quantity'] ?> x <?php echo $_POST['item_name'] ?> (<?php echo $_POST['item_number'] ?>)<br />
+					Paid by <?php echo $_POST['first_name'];?> <?php echo $_POST['last_name'];?> <?php echo $_POST['payer_business_name'];?> (<?php echo $_POST['payer_email'] ?>)
+					</p>
+		
 					<p><a href="#" onclick="javascript:window.open('https://www.paypal.com/us/cgi-bin/webscr?cmd=xpt/cps/popup/OLCWhatIsPayPal-outside','olcwhatispaypal','toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=400, height=350');"><img  src="https://www.paypal.com/en_US/i/logo/PayPal_mark_50x34.gif" border="0" alt="Paypal" style="float: right; margin-left:20px;"></a>When you click Buy Now you will be redirected to Paypal for secure online payment. Then we'll send you back here.</p>
 			</div><!-- .entry-content-->
 		</div><!-- .post -->
