d1b885d731cf93cd990086b00559863e0d39319d
ribcage <ribcage@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2008-01-15 15:41:22 +0000

    Changed because of Dave's suggestion.

--- templates/download.php
+++ templates/download.php
@@ -7,9 +7,9 @@
 				<img src="<?php release_cover_large (); ?>" style="margin-bottom: 20px; border: 1px solid #000;" />
 				<p>Thanks for your interest in <a href="<?php get_option('siteurl')?>/artists/<?php artist_slug(); ?>"><?php artist_name(); ?></a> and <a href="<?php get_option('siteurl');?>">Records On Ribs</a>!</p>
 		<p>
-		<a href="<?php release_download_link_mp3 (); ?>">Download</a> Zipped <a href="<?php get_option('siteurl')?>/help/formats/mp3">High Quality MP3</a> Files (<?php release_download_size_mp3() ?>).<br />
-		<a href="<?php release_download_link_flac (); ?>">Download</a> Zipped <a href="<?php get_option('siteurl')?>/help/formats/ogg">Ogg</a> Files (<?php release_download_size_ogg() ?>).<br />
-		<a href="<?php release_download_link_flac (); ?>">Download</a> Zipped <a href="<?php get_option('siteurl')?>/help/formats/flac">Flac</a> Files (<?php release_download_size_flac() ?>).
+		<a href="<?php release_download_link_mp3 (); ?>">Download</a> Zipped High Quality MP3 Files (<?php release_download_size_mp3() ?>).<br />
+		<a href="<?php release_download_link_flac (); ?>">Download</a> Zipped Ogg Files (<?php release_download_size_ogg() ?>).<br />
+		<a href="<?php release_download_link_flac (); ?>">Download</a> Zipped Flac Files (<?php release_download_size_flac() ?>).
 		</p>
 		<p>		<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">
 				<img alt="Creative Commons License" style="border-width:0; float:right;margin-left:20px;" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" />
