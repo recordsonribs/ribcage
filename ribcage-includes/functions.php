58585a3166be5854469a6d7cb80ab045608d66ab
ribcage <ribcage@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2008-02-16 16:52:34 +0000

    Hacky way of doing the same thing.

--- ribcage-includes/functions.php
+++ ribcage-includes/functions.php
@@ -56,10 +56,10 @@ function list_recent_releases_blurb ( $amount = 0 )
 	$now = gmdate('Y-m-d');
 
 	if ($amount) {
-		$releases = $wpdb->get_results("SELECT release_id FROM $wpdb->ribcage_releases WHERE release_date < $now ORDER BY release_id DESC LIMIT $amount ", ARRAY_A);
+		$releases = $wpdb->get_results("SELECT release_id FROM $wpdb->ribcage_releases WHERE release_released = 1 ORDER BY release_id DESC LIMIT $amount ", ARRAY_A);
 	}
 	else {
-		$releases = $wpdb->get_results("SELECT release_id FROM $wpdb->ribcage_releases WHERE release_date < $now ORDER BY release_id DESC", ARRAY_A);
+		$releases = $wpdb->get_results("SELECT release_id FROM $wpdb->ribcage_releases WHERE release_released = 1 ORDER BY release_id DESC", ARRAY_A);
 	}
 	
 	if (isset($releases)) {
