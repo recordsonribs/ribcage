e8cfff1d12f907d97ac3d2aa9e10a59d3c4a877d
ribcage <ribcage@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2008-07-11 19:02:34 +0000

    Made press page even more sexy!

--- templates/press.php
+++ templates/press.php
@@ -4,7 +4,8 @@
 		<div id="post-17" class="hentry p1 page publish author-alex category-uncategorized tag- y2007 m07 d19 h23">
 			<h2 class="entry-title"><a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>"><?php artist_name(); ?> - Press</a></h2>
 	<div class="entry-content">
-	<p><?php if (artist_picture_zip(0)) : ?><a href="<?php artist_picture_zip(); ?>">High Quality Press Photos (.zip)</a>
+	<p>If you require any further information regarding <a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>"><?php artist_name(); ?></a> or any of our other artists or releases, or wish to contact the artist directly, please do not hesitate to <a href="mailto:press@recordsonribs.com">e-mail press@recordsonribs.com</a>.</p>
+	<?php if (artist_picture_zip(0)) : ?><p><a href="<?php artist_picture_zip(); ?>">High Quality Press Photos (.zip)</a>
 	</p><?php endif ?>
 	<?php if (have_releases() ) : ?>
 	<h3>Releases</h3>
@@ -13,7 +14,7 @@
 		<a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>/<?php release_slug(); ?>"><img src="<?php release_cover_tiny ();?>" align="left" height="65px" width="65px" style="margin-right: 20px; border: 1px solid #000;" alt="<?php release_title(); ?>" /></a>
 	<?php $artist = get_artist($release['release_artist']); ?>
 	<p><a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>/<?php release_slug(); ?>"><?php release_title(); ?></a>
-	<br /><?php if (release_onesheet(0)) : ?><a href="<?php release_onesheet(); ?>">Information Sheet (.pdf)</a><?php endif ?><?php if (release_cover_huge(0)) : ?> - <a href="<?php release_cover_huge(); ?>">High Quality Cover Artwork<?php endif; ?></p>
+	<br /><?php if (release_onesheet(0)) : ?><a href="<?php release_onesheet(); ?>">Press Information Sheet (.pdf)</a><?php endif ?><?php if (release_cover_huge(0)) : ?> - <a href="<?php release_cover_huge(); ?>">High Quality Cover Artwork<?php endif; ?></p>
 	</div>
 	<?php endwhile; ?>
 	<?php endif; ?>
