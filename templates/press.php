30cd8a16392b7b9b157b1fef72bda5dbd6fd26d5
ribcage <ribcage@f5e22e0a-0f59-dc11-ae45-00188b745eb1>
2008-07-11 18:49:53 +0000

    Further press updates. Now added album artwork...

--- templates/press.php
+++ templates/press.php
@@ -10,7 +10,7 @@
 	<h3>Releases</h3>
 	<?php while ( have_releases () ) : the_release() ; ?>
 	<div class="ribcage-release">
-		<a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>/<?php release_slug(); ?>"><img src="<?php release_cover_tiny ();?>" align="left" style="margin-right: 20px; border: 1px solid #000;" alt="<?php release_title(); ?>" /></a>
+		<a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>/<?php release_slug(); ?>"><img src="<?php release_cover_tiny ();?>" align="left" height="65px" width="65px" style="margin-right: 20px; border: 1px solid #000;" alt="<?php release_title(); ?>" /></a>
 	<?php $artist = get_artist($release['release_artist']); ?>
 	<p><a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>/<?php release_slug(); ?>"><?php release_title(); ?></a>
 	<br /><?php if (release_onesheet(0)) : ?><a href="<?php release_onesheet(); ?>">Information Sheet (.pdf)</a><?php endif ?><?php if (release_cover_huge(0)) : ?> - <a href="<?php release_cover_huge(); ?>">High Quality Cover Artwork<?php endif; ?></p>
