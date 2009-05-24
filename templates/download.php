3a21448969dd820ed443f0551ea0ac5428efa26b
Alexander Lazarus <music@Albert-2.local>
2009-05-24 22:36:03 +0100

    Added Bittorrent template (rough)

--- templates/download.php
+++ templates/download.php
@@ -11,6 +11,16 @@
 		<a href="<?php release_download_link_ogg (); ?>">Download</a> Zipped Ogg Files (<?php release_download_size_ogg() ?>).<br />
 		<a href="<?php release_download_link_flac (); ?>">Download</a> Zipped Flac Files (<?php release_download_size_flac() ?>).
 		</p>
+		<h3>Bittorrent</h3>
+		<p>
+			The above files are also avaliable for download via Bittorrent. If you don't know what Bittorrent is, don't worry just use the links above. But if you do, please use Bittorrent as it will likely make your download considerably faster particularly at busy times, as well as lightening the load on our server. Once your download is completed, please continue to seed it as long as is possible.</p>
+		</p>
+		<p>
+		<a href="<?php release_download_link_mp3 (); ?>">Download Torrent</a> For High Quality MP3 Files.<br />
+		<a href="<?php release_download_link_ogg (); ?>">Download Torrent</a> For Ogg Files.<br />
+		<a href="<?php release_download_link_flac (); ?>">Download Torrent</a> For Flac Files.
+		</p>
+		<p>Torrent services provided by <a href="http://beta.legaltorrents.com/">LegalTorrents</a>.</p>
 		<p>		<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">
 				<img alt="Creative Commons License" style="border-width:0; float:right;margin-left:20px;" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" />
 				</a>This release is licensed under a 
