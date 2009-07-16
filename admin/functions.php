b6e66634d59995bf2d7800ebdbdca8c4dadf2948
recordsonribs <alex@recordsonribs.com>
2009-07-16 15:22:10 +0100

    Made add artist the same html code as manage artists as a prelude to combining them.

--- admin/functions.php
+++ admin/functions.php
@@ -46,120 +46,107 @@ EOT;
 	}
     ?>
 	<div class="wrap">
+			<div id="icon-options-general" class="icon32"><br /></div>
 		<h2>Add Artist</h2>
-			<form action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>" method="post" id="ribcage_add_artist" name="add_artist"> 
-				<fieldset>
-					<legend>Basic Details</legend>
-					<table class="optiontable">                     
+			<form action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>" method="post" id="ribcage_add_artist" name="add_artist">
+					<table class="form-table">             
 						<tr valign="top">
-							<th scope="row"><strong>Name: </strong></th> 
+							<th scope="row"><label for="artist_name">Name</label></th> 
 							<td>
-								<input type="text" style="width:320px;" class="cleardefault" value="<?php echo $artist_name_val; ?>" name="artist_name" id="artist_name" maxlength="200" />												
+								<input type="text" value="<?php artist_name(); ?>" name="artist_name" id="artist_name" class="regular-text"/>												
 							</td> 
 						</tr>
 						<tr valign="top">
-							<th scope="row"><strong>Sort Name: </strong></th> 
+							<th scope="row">Sort Name</th> 
 							<td>
-								<input type="text" style="width:320px;" class="cleardefault" value="<?php echo $artist_name_sort_val; ?>" name="artist_name_sort" id="artist_name_sort" />
+								<input type="text" value="<?php artist_name_sort(); ?>" name="artist_name_sort" id="artist_name_sort" class="regular-text" />
+								<span class="description">The name of the artist to be alphabetized. For example, 'Butterfly, The'.</span>
 							</td> 
 						</tr>
 						<tr valign="top">
-							<th scope="row"><strong>Artist Slug: </strong></th> 
+							<th scope="row">Artist Slug</th> 
 							<td>
-								<input type="text" style="width:320px;" class="cleardefault" value="<?php echo $artist_slug_val; ?>" name="artist_slug" id="artist_slug" />
+								<input type="text" style="width:320px;" class="regular-text code" value="<?php artist_slug(); ?>" name="artist_slug" id="artist_slug" /><span class="description">The URL you want for the artist - for example <?php echo get_option('siteurl'); ?>/artists/artist_slug</span>
 							</td> 
 						</tr>
 						<tr valign="top">
-							<th scope="row"><strong>Artist's Music Brainz ID: </strong></th> 
+							<th scope="row">Signup Date</th> 
 							<td>
-								<input type="text" style="width:320px;" class="cleardefault" value="<?php echo $artist_mbid_val; ?>" name="artist_mbid" id="artist_mbid" maxlength="50" />
+								<input type="text" style="width:100px;" class="regular-text code" value="<?php echo $artist['artist_signed']; ?>" name="artist_signed" id="artist_signed" maxlength="50" /><span class="description">The date the artist signed for your label</span>
 							</td> 
 						</tr>
 						<tr valign="top">
-							<th scope="row"><strong>Signup Date: </strong></th> 
+							<th scope="row">Creative Commons license</th> 
 							<td>
-								<input type="text" style="width:100px;" class="cleardefault" value="<?php echo $artist_signed_val; ?>" name="artist_signed" id="artist_signed" maxlength="50" />
+								<?php echo ribcage_cc_dropdown($artist_license_val); ?>
 							</td> 
 						</tr>
 						<tr valign="top">
-							<th scope="row"><strong>Creative Commons license: </strong></th> 
+							<th scope="row">Artist's Music Brainz ID</th> 
 							<td>
-								<?php echo ribcage_cc_dropdown(); ?>
+								<input type="text" style="width:320px;" class="regular-text code" value="<?php artist_musicbrainz (); ?>" name="artist_mbid" id="artist_mbid" maxlength="50" />
 							</td> 
 						</tr>
 						<tr valign="top">
-							<th scope="row"><strong>Artist Biography: </strong></th> 
+							<th scope="row">Website URL</th> 
 							<td>
-								<textarea rows="5" cols="50" name="artist_bio" id="artist_bio"><?php echo $artist_bio_val; ?></textarea>
+								<input type="text" style="width:320px;" class="regular-text code" value="<?php artist_website_link(); ?>" name="artist_link_website" id="artist_link_website" maxlength="200" />
 							</td> 
 						</tr>
 						<tr valign="top">
-							<th scope="row"><strong>Artist Picture 1: </strong></th> 
+							<th scope="row">MySpace URL</th> 
 							<td>
-								<input type="text" style="width:320px;" class="cleardefault" value="<?php echo $artist_picture_1_val; ?>" name="artist_picture_1" id="artist_picture_1" maxlength="200" />
+								<input type="text" style="width:320px;" class="regular-text code" value="<?php artist_myspace_link(); ?>" name="artist_link_myspace" id="artist_link_myspace" maxlength="200" />
 							</td> 
 						</tr>
 						<tr valign="top">
-							<th scope="row"><strong>Artist Picture 2: </strong></th> 
+							<th scope="row">Facebook URL</th> 
 							<td>
-								<input type="text" style="width:320px;" class="cleardefault" value="<?php echo $artist_picture_2_val; ?>" name="artist_picture_2" id="artist_picture_2" maxlength="200" />
+								<input type="text" style="width:320px;" class="regular-text code" value="<?php artist_facebook_link(); ?>" name="artist_link_facebook" id="artist_link_facebook" maxlength="200" />
 							</td> 
 						</tr>
 						<tr valign="top">
-							<th scope="row"><strong>Artist Picture 3: </strong></th> 
+							<th scope="row">Artist Biography</th> 
 							<td>
-								<input type="text" style="width:320px;" class="cleardefault" value="<?php echo $artist_picture_3_val; ?>" name="artist_picture_3" id="artist_picture_3" maxlength="200" />
+								<textarea rows="5" cols="50" name="artist_bio" id="artist_bio" class="regular-text"><?php echo $artist['artist_bio']; ?></textarea>
 							</td> 
 						</tr>
 						<tr valign="top">
-							<th scope="row"><strong>Artist Picture HQ Zipfile URL: </strong></th> 
+							<th scope="row">Short One Paragraph Description of Artist</th> 
 							<td>
-								<input type="text" style="width:320px;" class="cleardefault" value="<?php echo $artist_picture_zip_val; ?>" name="artist_picture_zip" id="artist_picture_zip" maxlength="200" />
+								<textarea rows="5" cols="50" name="artist_blurb_tiny" id="artist_blurb_tiny" class="regular-text"><?php echo $artist['artist_blurb_tiny']; ?></textarea>
 							</td> 
 						</tr>
 						<tr valign="top">
-							<th scope="row"><strong>One-sentence Description of Artist: </strong></th> 
+							<th scope="row">Artist Picture 1</th> 
 							<td>
-								<input type="text" style="width:320px;" class="cleardefault" value="<?php echo $artist_blurb_tiny_val; ?>" name="artist_blurb_tiny" id="artist_blurb_tiny" maxlength="255" />
+								<input type="text" style="width:320px;" class="regular-text code" value="<?php artist_picture_1(); ?>" name="artist_picture_1" id="artist_picture_1" maxlength="200" />
 							</td> 
 						</tr>
 						<tr valign="top">
-							<th scope="row"><strong>One-paragraph Description of Artist: </strong></th> 
+							<th scope="row">Artist Picture 2</th> 
 							<td>
-								<textarea rows="5" cols="50" name="artist_blurb_short" id="artist_blurb_short"><?php echo $artist_blurb_short_val; ?></textarea>
+								<input type="text" style="width:320px;" class="regular-text code" value="<?php echo $artist_picture_2_val; ?>" name="artist_picture_2" id="artist_picture_2" maxlength="200" />
 							</td> 
 						</tr>
 						<tr valign="top">
-							<th scope="row"><strong>Website URL: </strong></th> 
+							<th scope="row">Artist Picture 3</th> 
 							<td>
-								<input type="text" style="width:320px;" class="cleardefault" value="<?php echo $artist_link_website_val; ?>" name="artist_link_website" id="artist_link_website" maxlength="200" />
+								<input type="text" style="width:320px;" class="regular-text code" value="<?php echo $artist_picture_3_val; ?>" name="artist_picture_3" id="artist_picture_3" maxlength="200" />
 							</td> 
 						</tr>
 						<tr valign="top">
-							<th scope="row"><strong>MySpace URL: </strong></th> 
+							<th scope="row">Artist Picture HQ Zipfile URL</th> 
 							<td>
-								<input type="text" style="width:320px;" class="cleardefault" value="<?php echo $artist_link_myspace_val; ?>" name="artist_link_myspace" id="artist_link_myspace" maxlength="200" />
+								<input type="text" style="width:320px;" class="regular-text code" value="<?php echo $artist_picture_zip_val; ?>" name="artist_picture_zip" id="artist_picture_zip" maxlength="200" />
 							</td> 
-						</tr>
-						<tr valign="top">
-							<th scope="row"><strong>Facebook URL: </strong></th> 
-							<td>
-								<input type="text" style="width:320px;" class="cleardefault" value="<?php echo $artist_link_facebook_val; ?>" name="artist_link_facebook" id="artist_link_facebook" maxlength="200" />
-							</td> 
-						</tr>
-						<tr>
-							<th scope="row"></th> 
-							<td>
-							<p class="submit">													
-								<input type="submit" class="btn" name="save" style="padding:5px 30px 5px 30px;" value="<?php echo $button_name; ?>" />
-								<input type="hidden" name="<?php echo $hidden_field_name; ?>" value="Y">
-							</p>
-							</td>
 						</tr>
 					</table>
-				</fieldset>								
+					<p class="submit">
+						<input type="submit" name="Submit" class="button-primary" value="Save Changes" />
+						<input type="hidden" name="<?php echo $hidden_field_name; ?>" value="M">
+					</p>
 			</form>
-            </div>
 	</div>
 	<?php
 }
