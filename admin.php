4816e85dae1fa20e6fbae11902df185113b61adb
recordsonribs <alex@recordsonribs.com>
2009-07-16 21:15:45 +0100

    Gone back a bit

--- admin.php
+++ admin.php
@@ -13,8 +13,8 @@
  * @return void
  */
 function ribcage_admin_menu() { 	
-	add_menu_page('Ribcage', 'Ribcage', 8, dirname(__FILE__).'/admin.php');
-	add_submenu_page(dirname(__FILE__).'/admin.php', 'Ribcage', 'Add Artist', 8,'add_artist', 'ribcage_manage_artists');
+	add_menu_page('Ribcage', 'Ribcage', 8, dirname(__FILE__).'/admin.php', 'ribcage_admin_index');
+	add_submenu_page(dirname(__FILE__).'/admin.php', 'Ribcage', 'Add Artist', 8,'add_artist', 'ribcage_add_artist');
 	add_submenu_page(dirname(__FILE__).'/admin.php', 'Ribcage', 'Add Release', 8, 'add_release', 'ribcage_add_release');	
 	add_submenu_page(dirname(__FILE__).'/admin.php', 'Ribcage', 'Add Review', 8, 'add_review', 'ribcage_add_review');	
 	add_submenu_page(dirname(__FILE__).'/admin.php', 'Ribcage', 'Add Press', 8, 'add_press', 'ribcage_add_press');
