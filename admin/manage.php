bd892f688cac26b26ffd804cd8565de7c0e01326
recordsonribs <alex@recordsonribs.com>
2009-07-18 23:15:48 +0100

    Actually properly deletes now. Dangerous.

--- admin/manage.php
+++ admin/manage.php
@@ -72,6 +72,8 @@ function ribcage_manage_artists () {
 				$nonce=$_REQUEST['_wpnonce'];
 				if (! check_admin_referer('ribcage_manage')) die("Security check");
 				
+				delete_artist($_REQUEST['artist']);
+				
 				$message = 'deleted';
 				
 				$index = 1;
