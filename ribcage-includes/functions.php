7893282e1078c49dc9e04e15c770fdae46250b3d
recordsonribs <alex@recordsonribs.com>
2009-07-21 21:43:21 +0100

    Spelt my own name wrong.

--- ribcage-includes/functions.php
+++ ribcage-includes/functions.php
@@ -285,7 +285,7 @@ function ribcage_cc_dropdown($selected = false){
  * 
  * @param int $artist_id Artist ID for deletion.
  * @return bool True if this worked, false if it didn't.
- * @author Alexander Andres
+ * @author Alexander Andrews
  **/
 function delete_artist($artist_id)
 {
