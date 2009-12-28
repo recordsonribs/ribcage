72ee10053a203eae1aa31add1c5c3c133c9abb03
alex@recordsonribs.com <alex@recordsonribs.com>
2009-12-28 18:04:39 +0000

    Slight change to documentation.

--- ribcage.php
+++ ribcage.php
@@ -8,7 +8,7 @@
 	Author URI: http://recordsonribs.com
 */
 /*
-	Copyright 2009  Alexander Andrews  (email : alex@recordsonribs.com)
+	Copyright 2009  Alexander Andrews  (email: alex@recordsonribs.com)
 
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
