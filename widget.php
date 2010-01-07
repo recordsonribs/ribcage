df826f118a7e251babe97dbe048f2bafa07b91ca
alex@recordsonribs.com <alex@recordsonribs.com>
2010-01-07 07:46:12 +0000

    Bryan's revisions to the look and feel of the recent releases widget. Cheers!

--- widget.php
+++ widget.php
@@ -7,7 +7,6 @@
  * @package Ribcage
  * @subpackage Widgets
  **/
-
 class ribcage_widgets {	
 	/**
 	 * Registers Ribcage sidebar widgets and their controls.
@@ -25,28 +24,42 @@ class ribcage_widgets {
 	/**
 	 * Adds a widget for recent releases, the quantity of which is defined by an option.
 	 *
-	 * @author Alexander Andrews
+	 * Revisions and streamlining of CSS by Bryan Klausmeyer of Ivy Street http://ivystreet.net/. Thanks very much indeed.
 	 **/
 	function recent_releases ($args) {
 		global $releases, $release, $artist;
 	
 	    extract($args);
-		$releases = list_recent_releases_blurb('5');
-		$artists = list_artists_blurb();	
+		$releases = list_recent_releases_blurb('16');
+		$artists = list_artists_blurb();
 	?>
 	        <?php echo $before_widget; ?>
 	            <?php echo $before_title.'Recent Releases'.$after_title; ?>
 				<div class="textwidget" align="left">
 	            <?php while ( have_releases () ) : the_release() ; ?>	
 				<?php $artist = get_artist($release['release_artist']); ?>
-				<div style="margin-bottom:25px;font-size:12px;"><a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>/<?php release_slug(); ?>"><img src="<?php release_cover_tiny ();?>" align="right" style="margin-left: 10px; border: 1px solid #000;" height="65px" width="65px" alt="<?php release_title(); ?>" /></a>		
-				<a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>/" style="font-size:12px;"><?php artist_name(); ?></a> - <a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>/<?php release_slug(); ?>" style="font-size:12px;"><?php release_title(); ?></a><br />
-				<div style="font-size:9px;">
-				<a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>/<?php release_slug(); ?>" style="font-size:11px;color:black;">More</a> - <a href="javascript:popUp('<?php release_player_link (); ?>')" style="font-size:11px;color:black;">Listen</a> - <a href="<?php echo get_option('siteurl'); ?>/download/<?php release_slug(); ?>/" style="font-size:11px;color:black;">Download</a><?php if (release_physical()) : ?> - <a href="<?php get_option('siteurl'); ?>/buy/<?php release_product_id(); ?>" style="font-size:11px;color:black;">Buy</a><?php endif; ?>
-				</div>
-				</div>
+				<div class="artist_slug">
+					<a class="slug" href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>/<?php release_slug(); ?>"><img src="<?php release_cover_tiny ();?>" alt="<?php release_title(); ?>" /></a>
+					<div class="artist_slug_info">
+						<ul class="artist_slug_main">
+							<li class="artist"><a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>/"><?php artist_name(); ?></a><h2><a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>/<?php release_slug(); ?>"><?php release_title(); ?></a></h2></li>
+						</ul>
+						<ul class="artist_slug_meta">
+							<li class="more"><a href="<?php echo get_option('siteurl'); ?>/artists/<?php artist_slug(); ?>/<?php release_slug(); ?>">More</a></li>
+							<li class="listen"><a href="javascript:popUp('<?php release_player_link (); ?>')">Listen</a></li>
+						<?php if (release_physical()) : ?>
+							<li class="download"><a href="<?php echo get_option('siteurl'); ?>/download/<?php release_slug(); ?>/">Download</a></li>
+							<li class="last buy"><a href="<?php get_option('siteurl'); ?>/buy/<?php release_product_id(); ?>">Buy</a></li>
+						<?php else: ?>
+							<li class="last download"><a href="<?php echo get_option('siteurl'); ?>/download/<?php release_slug(); ?>/">Download</a></li>
+						<?php endif; ?>
+						</ul>
+					</div> <!-- end div.artist_slug_info -->
+					<div class="clear"></div>
+				</div> <!-- end div.artist_slug -->
 				<?php endwhile; ?>
-				<p><a href="<?php echo get_option('siteurl'); ?>/releases/">More Releases...</a></p>
+				<div class="clear"></div>
+				<p><a href="<?php echo get_option('siteurl'); ?>/releases/">more releases &rsaquo;</a></p>
 			</div>
 	        <?php echo $after_widget; ?>
 	<?php
