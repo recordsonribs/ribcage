872d7d8046fb139ed7aa4139302706884c25821e
alex@recordsonribs.com <alex@recordsonribs.com>
2010-01-15 20:37:09 +0000

    Revert "All pages should show up correctly in the navigation header."
    This reverts commit 0762622dc1ec8e6fb63942f2fa3b2999b8817a84.

--- ribcage.php
+++ ribcage.php
@@ -94,23 +94,20 @@ function ribcage_init (){
 		if (is_artist_page()){
 			switch ($wp_query->query_vars['artist_page']) {
 				case 'press':
-					$wp_query->query_vars['pagename'] = 'artists';
 					$releases = list_artist_releases ($artist['artist_id'], TRUE);
 					$load = ribcage_load_template('press.php');
 					break;
 					
 				case 'bio':
-					$wp_query->query_vars['pagename'] = 'artists';
 					$load  = ribcage_load_template('bio.php');
 					break;
 					
-				case 'feed':
+				case 'feed':		
 					$releases = list_artist_releases ($artist['artist_id']);
 					$load = ribcage_load_template ('feeds/artist-rss2.php');
 					break;
 					
 				default :
-					$wp_query->query_vars['pagename'] = 'releases';
 					$release = get_release_by_slug ($wp_query->query_vars['artist_page']);
 					$tracks = $release ['release_tracks'];
 					$reviews = $release['release_reviews'];
@@ -118,7 +115,6 @@ function ribcage_init (){
 			}
 		}
 		else {
-			$wp_query->query_vars['pagename'] = 'artists';
 			$releases = list_artist_releases ($artist['artist_id']);
 			$load = ribcage_load_template ('artist.php');
 		}
@@ -159,7 +155,6 @@ function ribcage_init (){
 			}
 			
 			else if ($wp_query->query_vars['format'] == 'skip') {
-				$wp_query->query_vars['pagename'] = 'releases';
 				$release = get_release_by_slug ($wp_query->query_vars['release_slug'], FALSE, FALSE);
 				$artist = get_artist ($release['release_artist']);
 				$load = ribcage_load_template('download.php');
@@ -179,8 +174,6 @@ function ribcage_init (){
 		else if (isset($wp_query->query_vars['release_slug'])) {
 			$release = get_release_by_slug ($wp_query->query_vars['release_slug'], FALSE, FALSE);
 			$artist = get_artist ($release['release_artist']);
-			
-			$wp_query->query_vars['pagename'] = 'releases';
 			
 			// If we haven't seen the user before, then nag them about the download.
 			if (!isset($_COOKIE["ask_donate"])){
@@ -234,8 +227,6 @@ function ribcage_init (){
 		
 		// Lookup the item they are looking for in the database.
 		$product = get_product($wp_query->query_vars['ribcage_product_id']);
-		
-		$wp_query->query_vars['pagename'] = 'releases';
 		
 		if (isset($wp_query->query_vars['ribcage_buy_mode'])){		
 			switch ($wp_query->query_vars['ribcage_buy_mode']) {			
