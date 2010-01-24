0cd921c937aba87fe8d72269c959c03432ecf5af
alex@recordsonribs.com <alex@recordsonribs.com>
2010-01-24 17:44:11 +0000

    Added documentation, audited and added proper Wordpress error names.

--- download.php
+++ download.php
@@ -6,14 +6,19 @@
  * @subpackage Downloads
  **/
 
-// download_release
-// Input the slug of the release, the desired format (flac/ogg/mp3)
-// Outputs the release as a file for download.
+/**
+ * Outputs the file selected from database to user for download.
+ *
+ * @author Alex Andrews
+ * @param string $release_slug The slug of the release to download.
+ * @param string $format The format of the release to download, flac|mp3|ogg
+ * @return void
+ */
 function download_release ($release_slug, $format) {
 	global $release;
 	
 	if (empty ($release_slug)) {
-		return new WP_Error ('no-release-to-download', __("You didn't specify a release to grab."));
+		return new WP_Error ('ribcage-no-release-to-download', __("You didn't specify a release to grab."));
 	}
 		
 	$release = get_release_by_slug ($release_slug, FALSE, FALSE);
@@ -34,21 +39,26 @@ function download_release ($release_slug, $format) {
 		$file = $release['release_flac'];
 	}
 	else {
-		return new WP_Error ('incorrect-format', __("$format isn't a format I am aware of.<br />Hence it isn't a format you can download, sorry."));
+		return new WP_Error ('ribcage-incorrect-format', __("$format isn't a format I am aware of.<br />Hence it isn't a format you can download, sorry."));
 	}
 	
 	ribcage_log();			
 	ribcage_download($file);
 }
 
-// download_track
-// Input the slug of the track, the desired format
-// Outputs the release as a file for download.
+/**
+ * Outputs an individual track for the user to download.
+ *
+ * @author Alex Andrews
+ * @param string $track_slug The slug of the track that is to be downloaded.
+ * @param string $format The format of the release to download, flac|mp3|ogg
+ * @return void
+ */
 function download_track ($track_slug, $format) {
 	global $track;
 	
 	if (empty ($track_slug)) {
-		return new WP_Error ('no-track-to-download', __("You didn't specify a track to grab."));
+		return new WP_Error ('ribcage-no-track-to-download', __("You didn't specify a track to grab."));
 	}
 	
 	$track = get_track_by_slug($track_slug);
@@ -67,18 +77,21 @@ function download_track ($track_slug, $format) {
 		$file = $track['track_flac'];
 	}
 	else {
-		return new WP_Error('incorrect-format', __("$format isn't a format I am aware of.<br />Hence it isn't a format you can download."));
+		return new WP_Error('ribcage-incorrect-format', __("$format isn't a format I am aware of.<br />Hence it isn't a format you can download."));
 	}
 	
 	ribcage_log (TRUE);	
 	ribcage_download($file);
 }
 
-// ribcage_download
-// Input the download file path (which is usually from the database).
-// Outputs the files for download.
-// Borrowed a good deal of code from Lester 'GaMerZ' Chan's WP-DownloadManager (http://lesterchan.net/portfolio/programming.php)
-// Mostly in the headers to output.
+/**
+ * Outputs a file of a given path to the user, setting the headers correctly so it pops up nicely.
+ *
+ * Borrowed a good deal of code from Lester 'GaMerZ' Chan's WP-DownloadManager (http://lesterchan.net/portfolio/programming.php)
+ *
+ * @author Alex Andrews
+ * @param string $file File path of the file to download
+ */
 function ribcage_download ($file) {
 	// The full path is the site root plus whatever is in the database.
 	$path = ABSPATH.$file;
