4c8fb12ee4acdb8e5545821c5746615fd7355df2
alex@recordsonribs.com <alex@recordsonribs.com>
2010-02-02 22:38:39 +0000

    Cheers to Domnic Fox for the Javascript problem.

--- templates/paypal-redirect.php
+++ templates/paypal-redirect.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 </script>
-<head profile="http://gmpg.org/xfn/11" onLoad="document.paypal.submit();">
+<head profile="http://gmpg.org/xfn/11">
 <title>Redirecting to PayPal . . .</title>
 <link rel="stylesheet" href="<?php echo get_option('siteurl')."/wp-content/plugins/ribcage/templates/css/redirect.css";?>" type="text/css" media="screen" />
 </head>
 
-<body>
+<body onLoad="document.paypal.submit();">
 
 <div class="logo">
 	<h1><span>Records on Ribs</span></h1>
