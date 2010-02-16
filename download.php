fa3ca871c938c0592a835529aa64a19664af6517
alex@recordsonribs.com <alex@recordsonribs.com>
2010-02-16 22:57:54 +0000

    DEEEbug.

--- download.php
+++ download.php
@@ -97,14 +97,14 @@ function ribcage_download ($file) {
 	$file = ABSPATH.$file;
 	
 	if (file_exists($file)) {
-	    header('Content-Description: File Transfer');
-	    header('Content-Type: application/octet-stream');
-	    header('Content-Disposition: attachment; filename='.basename($file));
-	    header('Content-Transfer-Encoding: binary');
-	    header('Expires: 0');
-	    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-	    header('Pragma: public');
-	    header('Content-Length: ' . filesize($file));
+	    echo('Content-Description: File Transfer');
+	    echo('Content-Type: application/octet-stream');
+	    echo('Content-Disposition: attachment; filename='.basename($file));
+	    echo('Content-Transfer-Encoding: binary');
+	    echo('Expires: 0');
+	    echo('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+	    echo('Pragma: public');
+	    echo('Content-Length: ' . filesize($file));
 	    ob_clean();
 	    flush();
 	    readfile($file);
