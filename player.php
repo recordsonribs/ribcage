28763b69d79d3ae7c327d474acf5a6a0cf384945
alex@recordsonribs.com <alex@recordsonribs.com>
2010-09-30 01:40:09 +0100

    Does this work just with the hard CSS reset?

--- player.php
+++ player.php
@@ -50,20 +50,6 @@ function show_player ($release_slug) {
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<script type="text/javascript" charset="utf-8">
-		function removejscssfile(filename, filetype){
-		 var targetelement=(filetype=="js")? "script" : (filetype=="css")? "link" : "none" //determine element type to create nodelist from
-		 var targetattr=(filetype=="js")? "src" : (filetype=="css")? "href" : "none" //determine corresponding attribute to test for
-		 var allsuspects=document.getElementsByTagName(targetelement)
-		 for (var i=allsuspects.length; i>=0; i--){ //search backwards within nodelist for matching elements to remove
-		  if (allsuspects[i] && allsuspects[i].getAttribute(targetattr)!=null && allsuspects[i].getAttribute(targetattr).indexOf(filename)!=-1)
-		   allsuspects[i].parentNode.removeChild(allsuspects[i]) //remove element by calling parentNode.removeChild()
-		 }
-		}
-
-		removejscssfile("index.include.815921251.css", "css") //remove all occurences of "somescript.js" on page
-		removejscssfile("default.include.587656512.js", "js") //remove all occurences "somestyle.css" on page
-	</script>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 <title>ROR Player</title>
 <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.8.1/build/reset/reset-min.css">
