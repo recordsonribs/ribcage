03467c02d986c0ece1f91660308be8f2505bcdde
Alex <alex@recordsonribs.com>
2011-08-05 23:53:26 +0100

    Migration file added.

---
+++ migrate.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Set of functions that migrates from Ribcage 1.0 to 2.0 converting database
+ * entires to custom post types.
+ *
+ * @author Alex Andrews <alex@recordonribs.com>
+ * @package Ribcage
+ * @subpackage Migration
+ * @version 1.0
+ * @since 2.0
+ */
+?>
