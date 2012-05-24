49db9f7d2561d48e8c6dd4445708869fe928962c
conatus <alex@recordsonribs.com>
2012-05-25 00:35:31 +0100

    Reverting as submodules have messed up all the things

---
+++ admin/buy.php
@@ -0,0 +1,294 @@
+<?php
+/**
+ * Product management
+ *
+ * @package Ribcage
+ * @subpackage Administration
+ */
+
+/**
+ * Manages products - adds, edits, deletes products.
+ *
+ * @return void
+ */
+function ribcage_manage_products () {
+	global $wpdb;
+	
+	global $products, $product;
+	global $releases, $release;
+	global $artist;
+	
+	// Security check
+	if (isset($_REQUEST['_wpnonce'])) {
+            if (wp_verify_nonce($nonce, 'ribcage_manage_releases')) {
+                die("Security check failed.");
+            }
+	}
+	
+	$nonce = wp_create_nonce ('ribcage_manage_products');
+	
+	if ($_REQUEST['ribcage_action']) {
+		array_pop($_POST);
+
+		$post_keys = array_keys($_POST);
+		$post_vals = array_values($_POST);
+
+		$string_keys = implode($post_keys,",");
+		$string_vals = "'".implode($post_vals,"','")."'";
+
+		$wpdb->show_errors();
+		
+		switch($_REQUEST['ribcage_action']) {
+			case 'add':
+				// Do we have all the fields we need to add something?
+				if ($_POST['product_name'] == null) {
+					$product = $_POST;
+					ribcage_edit_product_form('Sorry you missed the name of your product.');
+					return;
+				}
+				elseif ($_POST['product_cost'] == null) {
+					$product = $_POST;
+					ribcage_edit_product_form("Sorry you didn't set a cost for your product.");
+					return;
+				}
+				elseif ($_POST['product_description'] == null) {
+					$product = $_POST;
+					ribcage_edit_product_form("Sorry you didn't write a description for your product.");
+					return;
+				}
+				elseif (!is_numeric($_POST['product_cost'])){
+					$product = $_POST;
+					ribcage_edit_product_form("Sorry but that isn't a number for the cost of the product.");
+					return;
+				}
+				
+				// Do we already have a physical product for this release?
+				if ($_POST['product_related_release']) {
+					$sql = "SELECT release_physical FROM ".$wpdb->releases." WHERE release_id = ".$_POST['product_related_release'];
+					$check = $wpdb->get_row($sql, ARRAY_A);
+					
+					if ($check['release_physical'] == 1) {
+						$product = $_POST;
+						ribcage_edit_product_form("Sorry but that release already has a physical product associated with it.");
+						return;
+					}
+				}
+				
+				$sql = "INSERT INTO ".$wpdb->products."
+						($string_keys)
+						VALUES
+						($string_vals)";
+				$results = $wpdb->query($sql);
+				$wpdb->hide_errors();
+				
+				// Update our _ribcage_releases data with the correct details if we are relating this to a release.
+				if ($_POST['product_related_release']) {
+					$sql = "SELECT product_id FROM ".$wpdb->products." ORDER BY product_id DESC LIMIT 0,1";
+					$product = $wpdb->get_row($sql, ARRAY_A);
+					
+					$sql = "UPDATE ".$wpdb->releases." SET release_physical = 1, release_physical_cat_no ='".$product['product_id']."' WHERE release_id = ".$_POST['product_related_release'];
+					
+					$results = $wpdb->query($sql);
+				}
+				
+				$message = ' added';
+			break;
+			
+			case 'edit':
+				ribcage_edit_product_form();
+				return;
+			break;
+			
+			case 'edited':
+				// Do we have all the fields we need to add something?
+				if ($_POST['product_name'] == null) {
+					$product = $_POST;
+					ribcage_edit_product_form('Sorry you missed the name of your product.');
+					return;
+				}
+				elseif ($_POST['product_cost'] == null) {
+					$product = $_POST;
+					ribcage_edit_product_form("Sorry you didn't set a price for your product.");
+					return;
+				}
+				elseif ($_POST['product_description'] == null) {
+					$product = $_POST;
+					ribcage_edit_product_form("Sorry you didn't set a price for your product.");
+					return;
+				}
+				elseif (!is_numeric($_POST['product_cost'])){
+					$product = $_POST;
+					ribcage_edit_product_form("Sorry but that isn't a number for the cost of the product.");
+					return;
+				}
+				
+				$product_before = get_product($_REQUEST['product']);
+				$sql = "UPDATE ".$wpdb->prefix."ribcage_products
+						SET ";
+				
+				$i = 0;
+				
+				foreach($post_keys as $field):
+						$sql .= $field ."='".$post_vals[$i]."', ";
+						$i++;
+				endforeach;
+				
+				$sql .= " product_id = ".$_REQUEST['product']." 
+						WHERE product_id = ".$_REQUEST['product'];
+					
+				$results = $wpdb->query($sql);
+				$wpdb->hide_errors();
+				
+				// Update our _ribcage_releases data if we have changed the associated product.
+				if ($product_before['product_related_release'] != $_REQUEST['product_related_release']) {
+					// Delete it from our previous holder of the release.
+					$sql = "UPDATE ".$wpdb->prefix."ribcage_releases SET release_physical = 0, release_physical_cat_no = 0 WHERE release_id = ".$product_before['product_related_release'];
+					$results = $wpdb->query($sql);
+					
+					// Add it to the current holder of the release.
+					$sql = "UPDATE ".$wpdb->prefix."ribcage_releases SET release_physical = 1, release_physical_cat_no ='".$_REQUEST['product']."' WHERE release_id = ".$_POST['product_related_release'];	
+					$results = $wpdb->query($sql);	
+				}
+
+				$message = ' updated';
+			break;
+			
+			case 'delete':
+				$product_before = get_product($_REQUEST['product']);
+				delete_product($_REQUEST['product']);
+				
+				$sql = "UPDATE ".$wpdb->prefix."ribcage_releases SET release_physical = 0, release_physical_cat_no = 0 WHERE release_id = ".$product_before['product_related_release'];
+				$results = $wpdb->query($sql);
+				
+				$message = " deleted";
+			break;
+		}
+		
+		if (isset($message)){
+			echo '<div id="message" class="updated fade"><p><strong>Product '.$message.'.</strong></p></div>';
+		}
+	}
+	
+	register_column_headers('ribcage-manage-products',
+	array (
+		'cb'=>'<input type="checkbox" />',
+		'product_name'=> 'Product',
+		'local_downloads'=>'Related To Release'
+		));
+	
+	$products = list_products();
+	?>
+		<div class="wrap">
+			<div id="icon-plugins" class="icon32"><br /></div>
+			<h2>Manage Products</h2>
+				<form action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>" method="post" id="ribcage_manage_products" name="manage_artists"> 
+					<table class="widefat post fixed" cellspacing="0">
+							<thead>
+							<tr>
+							<?php print_column_headers('ribcage-manage-products'); ?>			
+							</tr>
+							</thead>
+							<tfoot>
+							<tr>			
+							<?php print_column_headers('ribcage-manage-products',FALSE); ?>	
+							</tr>
+							</tfoot>            
+							<tbody>
+								<?php while ( have_products () ) : the_product(); ?>
+								<?php $release = get_release($product['product_related_release']); ?>
+								<?php $artist['artist_name'] = get_artistname_by_id($release['release_artist']); ?>
+								<?php echo ($alt % 2) ? '<tr valign="top" class="">' : '<tr valign="top" class="alternate">'; ++$alt; ?>		
+								<th scope="row" class="check-column"><input type="checkbox" name="productcheck[]" value="2" /></th>
+								<td class="column-name"><strong><a class="row-title" href="?page=manage_products&ribcage_action=edit&product=<?php product_id(); ?>&amp;_wpnonce=<?php echo $nonce ?>" title="<?php product_name(); ?>" ><?php product_name(); ?></strong></a><br /><div class="row-actions"><span class='edit'><a href="?page=manage_products&ribcage_action=edit&product=<?php product_id(); ?>&amp;_wpnonce=<?php echo $nonce ?>">Edit</a> | </span><span class='delete'><a class='submitdelete' href="?page=manage_products&ribcage_action=delete&product=<?php product_id(); ?>&amp;_wpnonce=<?php echo $nonce ?>" onclick="if ( confirm('You are about to delete the product \'<?php product_name(); ?>\'\n  \'Cancel\' to stop, \'OK\' to delete.') ) { return true;}return false;">Delete</a></span></div></td>
+								<?php if ($product['product_related_release']) { ?>
+								<td class="column-name"><?php artist_name(); ?> - <?php release_title(); ?></td>
+								<?php } 
+								else { ?>
+								<td class="column-name">None.</td>
+								<?php } ?>
+								</tr>
+								<?php endwhile; ?>
+							</tbody>
+						</table>
+				</form>
+		</div>
+		<?php
+}
+
+/**
+ * Displays a form used for editing or adding a product to database.
+ *
+ * @param string $error Error message from the manage_products form.
+ * @param bool $related_to_release If true then we are passing from adding a release in general so we set the relation to this automatically
+ * @return void
+ */
+function ribcage_edit_product_form ($error = 0, $related_to_release = 0) {
+	global $artist;
+	global $release, $releases;
+	global $product;
+	
+	if (isset($_REQUEST['product'])) {
+		$product = get_product($_REQUEST['product']);
+	}
+
+	$releases = list_recent_releases_blurb();
+	
+	if ($error) {
+		echo '<div id="message" class="error"><p><strong>'.$error.'</strong></p></div>';
+	}
+	?>
+	<div class="wrap">
+		<div id="icon-options-general" class="icon32"><br /></div>
+		<?php if (isset($_REQUEST['product'])) { ?>
+		<h2>Editing <?php product_name(); ?></h2>
+		<form action="<?php get_option('siteurl'); ?>/wp-admin/admin.php?page=manage_products&product=<?php product_id() ?>&ribcage_action=edited" method="post" id="ribcage_edit_artist" name="edit_artist">
+		<?php } else { ?>
+		<h2>Add A Product</h2>
+		<form action="<?php get_option('siteurl'); ?>/wp-admin/admin.php?page=manage_products&ribcage_action=add" method="post" id="ribcage_edit_artist" name="edit_artist">
+		<?php }?>
+				<table class="form-table">             
+					<tr valign="top">
+						<th scope="row"><label for="product_name">Name</label></th> 
+						<td>
+							<input type="text" value="<?php product_name(); ?>" name="product_name" id="product_name" class="regular-text"/>												
+						</td> 
+					</tr>
+					<tr valign="top">
+						<th scope="row"><label for="product_name">Price</label></th> 
+						<td>
+							<input type="text" value="<?php echo $product['product_cost']; ?>" name="product_cost" id="product_cost" class="regular-text"/>												
+						</td> 
+					</tr>
+					<tr valign="top">
+						<th scope="row"><label for="product_related_release">Related To Release</label></th> 
+						<td>
+							<select name="product_related_release" id="product_related_release">
+								<option value = "">None</option>
+								<?php while ( have_releases () ) : the_release(); ?>
+								<?php $artist['artist_name'] = get_artistname_by_id($release['release_artist']); ?>
+								<?php if ($release['release_id'] == $product['product_related_release']) { ?>
+								<option selected value="<?php release_id();?>"><?php artist_name(); ?> - <?php release_title(); ?></option>
+								<?php } 
+								else { ?>
+								<option value="<?php release_id();?>"><?php artist_name(); ?> - <?php release_title(); ?></option>
+								<?php } ?>
+								<?php endwhile; ?>
+							</select>												
+						</td> 
+					</tr>
+					<tr valign="top">
+						<th scope="row"><label for="product_name">Product Description</label></th> 
+						<td>
+							<textarea rows="10" cols="100" name="product_description" id="product_description" class="regular-text"><?php product_description(); ?></textarea>					
+						</td> 
+					</tr>
+				</table>
+				<p class="submit">
+					<input type="submit" name="Submit" class="button-primary" value="Save Changes" />
+				</p>
+		</form>
+</div>
+	<?php
+}
+
+?>
---
+++ admin/functions.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * General functions for administration.
+ *
+ * @package Ribcage
+ * @subpackage Administration
+ **/
+
+/**
+ * Displays form for changing Ribcage options.
+ *
+ * @return void
+ **/
+function ribcage_options()
+{
+	?>
+	<div class="wrap">
+		<div id="icon-options-general" class="icon32"><br /></div>
+		<h2>Ribcage</h2>
+		<form method="post" action="options.php">
+		<table class="form-table">
+		<tr valign="top">
+		<th scope="row"><label for="ribcage_paypal_email">Paypal E-mail Address</label></th>
+		<td><input type="text" name="ribcage_paypal_email" value="<?php echo get_option('ribcage_paypal_email'); ?>" class="regular-text code"/><span class="description">E-mail to send money to from donations and purchases via Paypal</span></td>
+		</tr>
+		<tr valign="top">
+		<th scope="row"><label for="ribcage_image_location">Image Location</label></th>
+		<td><input type="text" name="ribcage_image_location" value="<?php echo get_option('ribcage_image_location'); ?>" class="regular-text code"/><span class="description">The directory to begin the directory tree of images for artists and release covers</class></td>
+		</tr>
+		<tr valign="top">
+		<th scope="row">File Location</th>
+		<td><input type="text" name="ribcage_file_location" value="<?php echo get_option('ribcage_file_location'); ?>" class="regular-text code"/><span class="description">The directory to begin the directory tree of files for audio and one sheets</class></td>
+		</tr>
+		<tr valign="top">
+		<th scope="row">Record Label Mark</th>
+		<td><input type="text" name="ribcage_mark" value="<?php echo get_option('ribcage_mark'); ?>" class="regular-text code"/><span class="description">The record label mark, Records On Ribs uses ROR, for example</span></td>
+		</tr>
+		<tr valign="top">
+		<th scope="row">Default Postage Within Your Country</th>
+		<td><input type="text" name="ribcage_postage_country" value="<?php echo get_option('ribcage_postage_country'); ?>" class="regular-text"/><span class="description">The default postage for a purchased item within the country you are in</span></td>
+		</tr>
+		<tr valign="top">
+		<th scope="row">Default Postage Worldwide</th>
+		<td><input type="text" name="ribcage_postage_worldwide" value="<?php echo get_option('ribcage_postage_worldwide'); ?>" class="regular-text"/><span class="description">The default postage for a purchased item worldwide</span></td>
+		</tr>
+		<tr valign="top">
+		<th scope="row">Press Contact</th>
+		<td><input type="text" name="ribcage_press_contact" value="<?php echo get_option('ribcage_press_contact'); ?>" class="regular-text code"/><span class="description">E-mail address people should contact for press enquires</span></td>
+		</tr>
+		</table>
+		<?php settings_fields('ribcage'); ?>
+		<p class="submit">
+		<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+		</p>
+		</form>
+		</div>
+	</div>
+	<?php
+}
+
+/**
+ * Adds the remote downloads from Legaltorrents.
+ *
+ * @param bool $echo If true the remote downloads are displayed.
+ * @return void
+ */
+function remote_downloads( $echo = TRUE )
+{
+	global $release;
+	
+	$total_downloads = 0;
+
+	$torrents = array (
+		'mp3'=>$release['release_torrent_mp3'],
+		'ogg'=>$release['release_torrent_ogg'],
+		'flac'=>$release['release_torrent_flac']
+	);
+
+	foreach ($torrents as $format => $val) {
+		
+		if ($val == NULL) {
+			$torrents[$format] = 0;
+			continue;
+		}
+		else {
+			$downloads = get_remote_downloads($val);
+				
+			if (is_wp_error($downloads)) {
+				echo $downloads->get_error_message();
+				continue;
+			}
+			
+			$torrents[$format] = $downloads;
+			
+			$total_downloads = $total_downloads + $downloads;
+		}
+	}
+		
+	if ( $echo ) {
+		?>
+		MP3: <?php echo number_format($torrents['mp3']); ?><br />
+		Ogg: <?php echo number_format($torrents['ogg']); ?><br />
+		Flac: <?php echo number_format($torrents['flac']); ?><br />
+		Total: <?php echo number_format($total_downloads);?>
+		<?php
+	}
+
+	return 	$total_downloads;
+}
+
+/**
+ * Gets the total downloads for a specific torrent on LegalTorrents.
+ *
+ * @param string $torrent The full URL of the torrent file we are getting.
+ * @return int Total number of download of the torrent.
+ */
+function get_remote_downloads ($torrent) 
+{
+	$torrent = str_replace(array('http://','https://'), '', $torrent);
+	$torrent = str_replace(array('www.legaltorrents.com/get/','beta.legaltorrents.com/get/','beta.legaltorrents.com/torrents/','.torrent'), '', $torrent);
+
+	$request_url = "http://www.legaltorrents.com/torrents/$torrent.xml";
+	$xml = simplexml_load_file($request_url);
+	
+	if (! $xml) {
+		return new WP_Error('ribcage-legaltorrents-xml-not-found', __("The xml file could not be found on Legaltorrents."));
+	}
+	
+	return $xml->{'totaldl'};
+}
+
+/**
+ * Displays a drop down menu of all the artists. 
+ *
+ * @param string $var_name Optional variable name which the dropdown corresponds to. For example release_artist is the default.
+ * @param int $artist_id Optional artist to make selected.
+ */
+function ribcage_artists_dropdown ($var_name = 'release_artist', $artist_id = null) {
+	global $artists, $artist;
+
+	$artists = list_artists_blurb();
+	$artist = $artists [$current_artist];
+	?>
+	<select name="<?php echo $var_name; ?>" id="<?php echo $var_name; ?>">
+	<?php while (have_artists ()) : the_artist(); ?>
+	<?php if ($artist['artist_id'] == $artist_id) { ?>
+	<option selected value="<?php artist_id();?>"><?php artist_name(); ?></option>
+	<?php } 
+	else { ?>
+	<option value="<?php artist_id();?>"><?php artist_name(); ?></option>
+	<?php } ?>
+	<?php endwhile; ?>
+	<?php
+}
+?>
