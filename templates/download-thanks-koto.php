b59c3b234cc57e43380687c5e4c583b5b67d3081
Alex <alex@recordsonribs.com>
2015-12-17 21:11:43 +0000

    Clarify special copy

--- templates/download-thanks-koto.php
+++ templates/download-thanks-koto.php
@@ -5,7 +5,9 @@
 			<!--<h2>Thanks <?php echo $_POST['first_name'];?>!</h2>-->
 			<div class="entry">
 				<img src="<?php release_cover_large (); ?>" alt="<?php release_title(); ?>"/>
-				<p>Thanks for your kind donation! Don't those downloads just sound so much better! We'd hug you if it wasn't for the restraining order!</p>
+				<p>Thanks for your kind donation!</p>
+				<p>We'll make sure the money gets to the <a href="http://www.unicef.org.uk/landing-pages/donate-syria/">UNICEF Christmas Appeal for Syrian Children</a></p>
+				<p>Enjoy!</p>
 				<ul class="download">
  					<li><a href="<?php get_option('siteurl');?><?php echo $release['release_mp3'];?>">Download</a> Zipped High Quality MP3 Files (<?php release_download_size_mp3(); ?>).</li>
  					<li><a href="<?php get_option('siteurl');?><?php echo $release['release_ogg'];?>">Download</a> Zipped Ogg Files (<?php release_download_size_ogg(); ?>).</li>
